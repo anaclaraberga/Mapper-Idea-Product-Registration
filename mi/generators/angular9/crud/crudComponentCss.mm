5a32f795b14d3baa52001698f7ba1ffc7e15243b
unknown <ana4ew.br@gmail.com>
2024-08-19 16:28:04 -0300

    Created: Crud Component CSS

---
+++ mi/generators/angular9/crud/crudComponentCss.mm
@@ -0,0 +1,33 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1723661398938" ID="ID_1159313010" LINK="../../../mapperidea.mm" MODIFIED="1724093519168" TEXT="crudComponentCss">
+<icon BUILTIN="element"/>
+<node CREATED="1723661746674" MODIFIED="1723661750506" POSITION="right" TEXT="patterns">
+<icon BUILTIN="element"/>
+<node CREATED="1723665125605" MODIFIED="1723665130874" TEXT="init">
+<icon BUILTIN="element"/>
+<node CREATED="1723665151093" MODIFIED="1723828398032" TEXT="button {&#xa;    margin-bottom: 30px;&#xa;}&#xa; ">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+</node>
+<node CREATED="1723646616505" MODIFIED="1723661676635" POSITION="right" TEXT="start">
+<icon BUILTIN="element"/>
+<node CREATED="1723646610576" MODIFIED="1723661703518" TEXT="match">
+<icon BUILTIN="element"/>
+<node CREATED="1723661754770" MODIFIED="1723661758775" TEXT="/classes">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+<node CREATED="1723646610576" MODIFIED="1723653796959" TEXT="body">
+<icon BUILTIN="element"/>
+<node CREATED="1723137437658" MODIFIED="1723483038922" TEXT="write-pattern">
+<icon BUILTIN="element"/>
+<node CREATED="1723137520274" MODIFIED="1723225575654" TEXT="init">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
